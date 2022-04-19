23d5270f3013169f69aea18ec85e5ba7b79cf24e
jiseooooon <57252187+jiseooooon@users.noreply.github.com>
2022-04-19 14:14:46 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,25 +28,25 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "pub-1" {
-  name = "${var.resource_prefix}-pub-1"
+  name = "${var.subnet_prefix}-pub-1"
   ip_cidr_range = "192.168.1.0/24"
   network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "pub-2" {
-  name = "${var.resource_prefix}-pub-2"
+  name = "${var.subnet_prefix}-pub-2"
   ip_cidr_range = "192.168.2.0/24"
   network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "prv-1" {
-  name = "${var.resource_prefix}-prv-1"
+  name = "${var.subnet_prefix}-prv-1"
   ip_cidr_range = "192.168.3.0/24"
   network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "prv-2" {
-  name = "${var.resource_prefix}-prv-2"
+  name = "${var.subnet_prefix}-prv-2"
   ip_cidr_range = "192.168.4.0/24"
   network = google_compute_network.vpc_network.id
 }
